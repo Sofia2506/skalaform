26cd0d0bfce77660591b584042fe909901b8b778
Sofia2506 <sonque@unsa.edu.pe>
2023-01-28 10:31:05 -0500

    Cambio de imagen listo

--- ajax.js
+++ ajax.js
@@ -1,29 +1,24 @@
 var xhttp = new XMLHttpRequest();
-var nom = document.getElementById("pago").value;
-var activities = document.getElementById("pago")
+var tipoPago = document.getElementById("pago");
+var img = document.getElementById("pago-imagen");
 let html="";
-xhttp.onreadystatechange = function() {
+/* xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         console.log(nom);
-        html +=`<p>${nom}</p>`;
-        document.getElementById("barra").innerHTML = html;
     }
 }
 xhttp.open("GET", "", true);
-xhttp.send();
+xhttp.send(); */
 
-activities.addEventListener("click", function() {
-    var options = activities.querySelectorAll("option");
-    var count = options.length;
-    if(typeof(count) === "undefined" || count < 2)
-    {
-        addActivityItem();
+tipoPago.addEventListener("change", function() {
+/*     html +=`<p>${tipoPago.value}</p>`;
+    document.getElementById("barra").innerHTML = html; */
+    if(tipoPago.value == "Yape"){
+        console.log("Yape");
+        img.src = "img/yape-skala.png";
     }
-});
-
-activities.addEventListener("change", function() {
-    if(activities.value == "addNew")
-    {
-        addActivityItem();
+    else{
+        console.log("Deposito");
+        img.src = "img/deposito.png";
     }
 });
