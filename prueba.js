1ed5a9bccdc5dbd07a46e5b0bba3a7023e9d1fec
Sofia2506 <sonque@unsa.edu.pe>
2023-01-30 13:43:25 -0500

    Botones logica

--- prueba.js
+++ prueba.js
@@ -1,18 +1,37 @@
 var cant = 0;
 var pregunta = document.getElementsByClassName("paso");
-console.log("weon");
-console.log(pregunta);
-function siguiente(){
-    console.log("Siguiente");
-    cant+=100;
+var antes = document.getElementById("anterior");
+var next = document.getElementById("siguiente");
+var enviar = document.getElementById("enviar");
+function avanzar(){
     for(let i = 0 ; i<pregunta.length; i++){
         pregunta[i].style.transform="translateX(-"+cant+"%)";
     }
+    visibilidad();
+}
+function siguiente(){
+    cant+=100;
+    avanzar();
+    console.log(cant);
 }
 function anterior(){
-    console.log("Anterior");
     cant-=100;
-    for(let i = 0 ; i<pregunta.length; i++){
-        pregunta[i].style.transform="translateX(-"+cant+"%)";
+    avanzar();
+    console.log(cant);
+}
+function visibilidad(){
+    if(cant == 300){
+        next.style.visibility="hidden";
+        enviar.style.visibility="visible";
+    }
+    else{
+        next.style.visibility="visible"
+        enviar.style.visibility="hidden";
+    }
+    if(cant == 0){
+        antes.style.visibility="hidden";
+    }
+    else{
+        antes.style.visibility="visible";
     }
 }
